7359250201f3143f0a9ccf811ce8f517e3571b40
timvanklaveren <timvanklaveren@gmail.com>
2016-02-15 00:08:15 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,7 +61,7 @@ function respond() {
   } 
   else if(request.text && botRegexhelp.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("**List of commands for Fedora Bot.**        */swag* */420* */cool guy* */hank* */tim* */luke* */ayy* */trevor* */tygr* */sex* */like* *decoy* */cole*. **Message Tim to add more commands.**"+request.text.substring(5,8)+"/depthchart");
+    postMessage("**List of commands for Fedora Bot.**        **/swag   /420    /cool guy    /hank    /tim    /luke    /ayy    /trevor    /tygr    /sex    /like    /decoy    /cole**. If you wanna share a twitch.tv link type /twitch then enter channel name and bot will auto complete link. **Message Tim to add more commands.**"+request.text.substring(5,8)+"/depthchart");
     this.res.end();
   } 
   else if(request.text && botRegextim.test(request.text)) {
