f45ee5d2beef2f67d12ee0281e209e4c96008153
timvanklaveren <timvanklaveren@gmail.com>
2016-02-18 01:58:40 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -318,7 +318,95 @@ function postMessage(response) {
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
-}
+
+    "data": [
+        {
+            "type": "gif",
+            "id": "sj0sbNi9cv2dG",
+            "slug": "cat-transparent-sj0sbNi9cv2dG",
+            "url": "http://giphy.com/gifs/cat-transparent-sj0sbNi9cv2dG",
+            "bitly_gif_url": "http://gph.is/Kq60z1",
+            "bitly_url": "http://gph.is/Kq60z1",
+            "embed_url": "http://giphy.com/embed/sj0sbNi9cv2dG",
+            "username": "",
+            "source": "http://ibehhvinny.tumblr.com/post/71539185754/so-coot",
+            "rating": "g",
+            "caption": "",
+            "content_url": "",
+            "source_tld": "www.tumblr.com",
+            "source_post_url": "http://www.tumblr.com",
+            "import_datetime": "2014-01-04 15:39:18",
+            "trending_datetime": "1970-01-01 00:00:00",
+            "images": {
+                "fixed_height": {
+                    "url": "http://media2.giphy.com/media/sj0sbNi9cv2dG/200.gif",
+                    "width": "193",
+                    "height": "200",
+                    "mp4": "http://media.giphy.com/media/sj0sbNi9cv2dG/200.mp4"
+                },
+                "fixed_height_still": {
+                    "url": "http://media2.giphy.com/media/sj0sbNi9cv2dG/200_s.gif",
+                    "width": "193",
+                    "height": "200"
+                },
+                "fixed_height_downsampled": {
+                    "url": "http://media2.giphy.com/media/sj0sbNi9cv2dG/200_d.gif",
+                    "width": "193",
+                    "height": "200"
+                },
+                "fixed_width": {
+                    "url": "http://media2.giphy.com/media/sj0sbNi9cv2dG/200w.gif",
+                    "width": "200",
+                    "height": "207",
+                    "mp4": "http://media.giphy.com/media/sj0sbNi9cv2dG/200w.mp4"
+                },
+                "fixed_width_still": {
+                    "url": "http://media2.giphy.com/media/sj0sbNi9cv2dG/200w_s.gif",
+                    "width": "200",
+                    "height": "207"
+                },
+                "fixed_width_downsampled": {
+                    "url": "http://media2.giphy.com/media/sj0sbNi9cv2dG/200w_d.gif",
+                    "width": "200",
+                    "height": "207"
+                },
+                "downsized": {
+                    "url": "http://media2.giphy.com/media/sj0sbNi9cv2dG/giphy.gif",
+                    "width": "400",
+                    "height": "414",
+                    "size": "422870"
+                },
+                "downsized_still": {
+                    "url": "http://media2.giphy.com/media/sj0sbNi9cv2dG/giphy_s.gif",
+                    "width": "400",
+                    "height": "414"
+                },
+                "original": {
+                    "url": "http://media2.giphy.com/media/sj0sbNi9cv2dG/giphy.gif",
+                    "width": "400",
+                    "height": "414",
+                    "size": "422870",
+                    "frames": "39",
+                    "mp4": "http://media.giphy.com/media/sj0sbNi9cv2dG/giphy.mp4"
+                },
+                "original_still": {
+                    "url": "http://media2.giphy.com/media/sj0sbNi9cv2dG/giphy_s.gif",
+                    "width": "400",
+                    "height": "414"
+                }
+            }
+        },
+        ... 24 more items
+    ],
+    "meta": {
+        "status": 200,
+        "msg": "OK"
+    },
+    "pagination": {
+        "total_count": 573,
+        "count": 25,
+        "offset": 0
+    }
 
 
 exports.respond = respond;
